6048bf3e4215014727397113da733aa918d1c063
Tariq Saeed <tariqsaeed@Tariqs-MacBook-Pro.local>
2024-05-18 20:54:10 +0300

    code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <h1>Hello World!</h1>
   `,
-  styles: [],
 })
-export class AppComponent {
-  title = 'tariq';
-}
+export class AppComponent {}
